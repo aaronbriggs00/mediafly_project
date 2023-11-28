cbc4f478628786100dc5cc92c3cdbc8ea691d210
aaron briggs <aaron00.briggs@gmail.com>
2023-11-28 16:13:22 -0600

    creates basic user model, controller; sessions controller, & auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      render json: @user, status: :created
+    else  
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+end
